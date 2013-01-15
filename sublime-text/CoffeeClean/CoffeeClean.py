d607763c8583daf159b35a8db3c014b716149741
Chris Breiding <chrisbreiding@gmail.com>
2013-01-15 16:27:00 -0500

    Add sublime text package

---
+++ sublime-text/CoffeeClean/CoffeeClean.py
@@ -0,0 +1,57 @@
+import sublime_plugin
+
+
+class CoffeeCleanCommand(sublime_plugin.EventListener):
+
+    def on_pre_save(self, view):
+        if len(view.file_name()) > 0 and view.file_name().endswith((".coffee")):
+            print('cleaning coffee')
+
+            edit = view.begin_edit()
+
+            replacements = [
+                # missing spaces
+                ['(?<=\S),(?=\S)', ', '],
+                [':(?=[(@=\'"-])', ': '],
+                ['=\{', '= {'],
+                ['=\[', '= ['],
+                ['=\(', '= ('],
+                ['=\(', '= ('],
+                ['=\'', '= \''],
+                ['="', '= "'],
+                ['(?<!=)(?<=\S)=', ' ='],
+
+                # extra spaces
+                ['[ \t]+=[ \t]+', ' = '],
+                ['(?<=\S)[ \t]+,', ','],
+                ['\[[ \t]+(?=\S)', '['],
+                ['(?<=\S)[ \t]+\]', ']'],
+                ['\([ \t]+(?=\S)', '('],
+                ['(?<=\S)[ \t]+\)', ')'],
+                ['\)\s+->', ')->'],
+                ['\)\s+=>', ')=>'],
+
+                # ensure a space before a lambda in parens
+                ['\((?=\([a-zA-Z0-9,]+\)-)', '( '],
+                ['\((?=\([a-zA-Z0-9,]+\)=)', '( '],
+
+                # quotes
+                ['""', "''"],
+
+                # misc
+                ["'use strict';", "'use strict'"],
+                ['"use strict";', "'use strict'"],
+                ['@$', 'this']
+            ]
+
+            for replacement in replacements:
+                find = replacement[0]
+                replace = replacement[1]
+
+                regions = view.find_all(find)
+
+                if regions:
+                    for region in reversed(regions):
+                        view.replace(edit, region, replace)
+
+            view.end_edit(edit)
